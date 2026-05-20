18f6ba5408546661ffc404512bdd9cc684b2954a
KKKG100 <122702920+KKKG100@users.noreply.github.com>
2026-05-19 21:46:52 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,31 +4,75 @@ const WebSocket = require("ws")
 const path = require("path")
 
 const app = express()
+
 const server = http.createServer(app)
-const wss = new WebSocket.Server({ server })
+
+const wss = new WebSocket.Server({
+    server
+})
 
 app.use(express.static(path.join(__dirname, "public")))
 
 let latestPose = {}
 
+-----------------------------------------------------
+-- WEBSOCKET
+-----------------------------------------------------
+
 wss.on("connection", (ws) => {
-    console.log("Browser connected")
+
+    console.log("WebSocket connected")
 
     ws.on("message", (message) => {
+
         try {
-            latestPose = JSON.parse(message.toString())
+
+            latestPose = JSON.parse(
+                message.toString()
+            )
+
         } catch (err) {
+
             console.log(err)
+
         }
+    })
+
+    ws.on("close", () => {
+
+        console.log("WebSocket disconnected")
+
     })
 })
 
+-----------------------------------------------------
+-- POSE ENDPOINT
+-----------------------------------------------------
+
 app.get("/pose", (req, res) => {
+
     res.json(latestPose)
+
 })
 
-const PORT = 3000
+-----------------------------------------------------
+-- TEST
+-----------------------------------------------------
+
+app.get("/test", (req, res) => {
+
+    res.send("Server works")
+
+})
+
+-----------------------------------------------------
+-- START
+-----------------------------------------------------
+
+const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`)
+
+    console.log("Running on port " + PORT)
+
 })
